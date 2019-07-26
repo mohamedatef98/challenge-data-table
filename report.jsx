b1471f5f90d6b3832600b0f1cd5cc56c921f6842
Mohamed Atef <eng.mohamedatef98@gmail.com>
2019-07-27 00:59:57 +0200

    Add ReactPivot Jsx Component

--- report.jsx
+++ report.jsx
@@ -4,8 +4,32 @@ var createReactClass = require('create-react-class')
 
 var rows = require('./data.json')
 
+var dimensions = []
+
+var reduce = function(row, memo){
+
+
+  return memo
+}
+
+
+var calculations = [
+
+]
+
 module.exports = createReactClass({
   render () {
-    return <div>Report</div>
+    return (
+    <div>
+      <h1>Report</h1>
+      <ReactPivot 
+        rows={rows}
+        dimensions={dimensions}
+        reduce={reduce}
+        calculations={calculations}
+        activeDimensions={[]}
+      />
+    </div>
+    )
   }
 })
