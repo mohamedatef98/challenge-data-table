f046640448353f0a62a2db01a37b40e5a81a680a
Mohamed Atef <eng.mohamedatef98@gmail.com>
2019-07-27 01:12:40 +0200

    Show Displays

--- report.jsx
+++ report.jsx
@@ -17,13 +17,17 @@ var reduce = function(row, memo){
   var currentLoads = (memo.loads || 0)
   memo.loads = row.type === 'load' ? currentLoads + 1 : currentLoads
 
+  var currentDisplays = (memo.displays || 0)
+  memo.displays = row.type === 'display' ? currentDisplays + 1 : currentDisplays
+
   return memo
 }
 
 
 var calculations = [
   {title: 'Impressions', value: function(memo) {return memo.impressions}},
-  {title: 'Loads', value: function(memo) {return memo.loads}}
+  {title: 'Loads', value: function(memo) {return memo.loads}},
+  {title: 'Displays', value: function(memo) {return memo.displays}},
 ]
 
 module.exports = createReactClass({
