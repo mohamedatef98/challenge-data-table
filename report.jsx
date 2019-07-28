a4a5b811edaea0d551940d1ed711513a11107f69
Mohamed Atef <eng.mohamedatef98@gmail.com>
2019-07-28 09:49:49 +0200

    Use Counts value as string not function

--- report.jsx
+++ report.jsx
@@ -26,9 +26,9 @@ var reduce = function(row, memo){
 
 var calculations = [
   //Impressions, Loads, Displays Count
-  {title: 'Impressions', value: function(memo) {return memo.impressions}},
-  {title: 'Loads', value: function(memo) {return memo.loads}},
-  {title: 'Displays', value: function(memo) {return memo.displays}},
+  {title: 'Impressions', value: 'impressions'},
+  {title: 'Loads', value: 'loads'},
+  {title: 'Displays', value: 'displays'},
 
   //Load Rate (loads/impressions) %, Display Rate (displays/loads) %
   {title: 'Load Rate', value: function(memo) {return memo.loads*100.0/memo.impressions}, template: function(val) {return val.toFixed(1)+"%"}},
